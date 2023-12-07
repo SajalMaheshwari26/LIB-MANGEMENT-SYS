9c875db177ca5b977131e231bb5da5b74990231e
SajalMaheshwari26 <82907617+SajalMaheshwari26@users.noreply.github.com>
2023-12-07 12:06:12 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,20 @@
 import requests
 import json  # Add this line to import the 'json' module
 
-# Example data for a new book
+
+new_book_data = {
+    'title': 'The hell in the nightw ',
+    'author': 'JDs. Salinger'
+}
+
+# Make a POST request to add a new book
+response = requests.post('http://localhost:5000/api/books', json=new_book_data)
+print(response.json())
 updated_book_data = {
-    'title': 'New Title',
-    'author': 'New Author'
+    'title': 'New Titlest',
+    'author': 'Newestest'
 }
 
 # Make a PUT request to update a book with id=1
 response = requests.put('http://localhost:5000/api/books/1', json=updated_book_data)
-print(response)
+print(response.json())
